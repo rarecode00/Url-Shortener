807c26fdb63c9401565711649d4eeb22e083f169
Krishna Sharma <Ks9192174414@gmail.com>
2024-06-30 14:24:44 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layout/app-layout";
+import Dashboard from "./pages/dashboard";
+import AuthPage from "./pages/auth";
+import LinkPage from "./pages/link";
+import RedirectPage from "./pages/redirect";
+import LandingPage from "./pages/landing-page";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <AuthPage />,
+      },
+      {
+        path: "/link/:id",
+        element: <LinkPage />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="text-red">This is the App.js</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
